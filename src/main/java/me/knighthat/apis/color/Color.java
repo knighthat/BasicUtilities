4b86306a7970dddf60fff380bef16b67afe7f133
knighthat <knightchanelgaming@gmail.com>
2022-06-09 11:24:15 -0500

    0.1

---
+++ src/main/java/me/knighthat/apis/color/Color.java
@@ -0,0 +1,29 @@
+package me.knighthat.apis.color;
+
+import lombok.NonNull;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.ChatColor;
+
+public interface Color {
+
+    default @NonNull String colorString(@NonNull String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    default @NonNull Component color(@NonNull String s) {
+        return Component.text(colorString(s));
+    }
+
+    default @NonNull Component stripColor(@NonNull Component c) {
+
+        String stripped = ChatColor.stripColor(colorString(toString(c)));
+        return Component.text(stripped);
+    }
+    
+    default @NonNull String toString(@NonNull Component c) {
+        return PlainTextComponentSerializer.plainText().serialize(c);
+    }
+
+
+}
